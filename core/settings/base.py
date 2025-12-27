ad516caa0e402be2a4533594fcb0cbd28440bd7d
Shreeraj Jadhav <jadhavshreeraj2020@gmail.com>
2025-12-27 23:55:24 +0530

    Fix BASE_DIR path for templates

--- core/settings/base.py
+++ core/settings/base.py
@@ -7,7 +7,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # ✅ CORRECT (Standard Django structure)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # =========================
