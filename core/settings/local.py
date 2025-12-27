f751ff9e0711863ae87ad50d71b35ead5923c803
Shreeraj Jadhav <jadhavshreeraj2020@gmail.com>
2025-12-27 23:11:20 +0530

    Fix allowed hosts

--- core/settings/local.py
+++ core/settings/local.py
@@ -4,6 +4,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "web-production-5294.up.railway.app",  # <--- Add this!
+    '.railway.app',
     ".vercel.app",                         # <--- Add this for frontend!
     "127.0.0.1",
     "localhost",
