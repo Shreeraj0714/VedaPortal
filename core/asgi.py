9a774707d038d99818853d575103f9e6e7551524
Shreeraj Jadhav <jadhavshreeraj2020@gmail.com>
2025-12-27 20:08:10 +0530

    Fix ASGI settings override and Railway ALLOWED_HOSTS

--- core/asgi.py
+++ core/asgi.py
@@ -3,36 +3,24 @@ ASGI config for core project.
 """
 
 import os
-import django
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
 import main.routing
 
 # --------------------------------------------------
-# FORCE Django settings module (OVERRIDES EVERYTHING)
+# DO NOT hardcode settings module
+# Let Railway ENV decide
 # --------------------------------------------------
-os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings.local"
 
-django.setup()
-
-# --------------------------------------------------
-# Django ASGI application
-# --------------------------------------------------
 django_asgi_app = get_asgi_application()
 
-# --------------------------------------------------
-# Protocol router
-# --------------------------------------------------
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
-            URLRouter(
-                main.routing.websocket_urlpatterns
-            )
+            URLRouter(main.routing.websocket_urlpatterns)
         ),
     }
 )
+
