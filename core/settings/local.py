5ed334562c83c5eca41266d85af38e56f46d9384
Shreeraj Jadhav <jadhavshreeraj2020@gmail.com>
2025-12-27 20:14:46 +0530

    Update local settings with allowed hosts

--- core/settings/local.py
+++ core/settings/local.py
@@ -3,6 +3,8 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "web-production-5294.up.railway.app",  # <--- Add this!
+    ".vercel.app",                         # <--- Add this for frontend!
     "127.0.0.1",
     "localhost",
 ]
